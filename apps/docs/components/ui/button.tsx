8d450f55dc716fc159209d594b5d284e11f97ea0
Mark Otto <markdotto@gmail.com>
2026-05-06 14:30:33 -0700

    More consistent button to button group styles (#643)

--- apps/docs/components/ui/button.tsx
+++ apps/docs/components/ui/button.tsx
@@ -19,7 +19,7 @@ const buttonVariants = cva(
         secondary:
           'bg-secondary text-secondary-foreground hover:bg-secondary/80',
         muted:
-          'bg-secondary/60 text-accent-foreground/75 hover:bg-secondary/70 hover:text-accent-foreground',
+          'bg-secondary text-accent-foreground/75 hover:text-accent-foreground',
         ghost:
           'border bg-transparent border-transparent hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50',
         link: 'text-primary underline-offset-4 hover:underline',
