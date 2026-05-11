58b4d05f126b32d1d2cf0357b2299705e679c690
Amadeus Demarzi <amadeus@users.noreply.github.com>
2026-05-11 15:47:46 -0700

    [diffs] Remove old /gh route (#666)
    * Remove old /gh route
    
    Now it should just redirect to diffshub.com, since that site is in a
    pretty good spot
    
    * review fix

--- apps/docs/app/gh/page.tsx
+++ apps/docs/app/gh/page.tsx
@@ -1,17 +1,5 @@
-'use client';
+import { permanentRedirect } from 'next/navigation';
 
-import { WorkerPoolContext } from '@diffs/_components/WorkerPoolContext';
-
-import { GHViewer } from './GHViewer';
-import { Header } from '@/components/Header';
-
-export default function AdvancedDiffPage() {
-  return (
-    <WorkerPoolContext>
-      <div className="flex h-dvh flex-col">
-        <Header className="mx-0 px-5 md:px-5" />
-        <GHViewer />
-      </div>
-    </WorkerPoolContext>
-  );
+export default function GitHubRedirectPage() {
+  permanentRedirect('https://diffshub.com');
 }
