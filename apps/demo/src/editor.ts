08b1da15322e724993c184980b9edd210c25dc27
Je Xia <i@jex.me>
2026-05-18 20:29:40 +0800

    Update edtior demo app

--- apps/demo/src/editor.ts
+++ apps/demo/src/editor.ts
@@ -33,7 +33,90 @@ const API = {
 
 const fileTreeContainer = document.getElementById('file-tree-container')!;
 const editorContainer = document.getElementById('editor-container')!;
-const editor = new Editor<undefined>();
+const editor = new Editor<undefined>({
+  enabledQuickEdit: true,
+  renderQuickEdit: ({ close, replaceSelectionText }) => {
+    const el = document.createElement('div');
+    const input = document.createElement('input');
+    const span = document.createElement('span');
+    const left = document.createElement('div');
+    const right = document.createElement('div');
+    Object.assign(input.style, {
+      position: 'absolute',
+      top: '0',
+      left: '0',
+      width: '100%',
+      height: '24px',
+      padding: '0',
+      margin: '0',
+      lineHeight: '24px',
+      border: 'none',
+      outline: 'none',
+      resize: 'none',
+    });
+    input.placeholder = 'Ask AI...';
+    input.addEventListener('keyup', (e) => {
+      if (e.key === 'Enter') {
+        e.preventDefault();
+        input.style.opacity = '0';
+        span.style.opacity = '1';
+        right.style.opacity = '0.5';
+        setTimeout(() => {
+          close();
+          replaceSelectionText('');
+        }, 1500);
+      }
+    });
+    Object.assign(el.style, {
+      boxSizing: 'border-box',
+      width: '100%',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      margin: '8px 0',
+      padding: '8px',
+      border: '1px solid rgba(128,128,128,0.5)',
+      borderRadius: '6px',
+    });
+    Object.assign(span.style, {
+      position: 'absolute',
+      top: '0',
+      left: '0',
+      lineHeight: '24px',
+      color: '#999',
+      opacity: '0',
+    });
+    Object.assign(left.style, {
+      position: 'relative',
+      flex: '1',
+      height: '24px',
+    });
+    Object.assign(right.style, {
+      width: '20px',
+      height: '20px',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      color: '#666',
+    });
+    span.textContent = 'Thinking...';
+    right.innerHTML = `
+      <svg width="20" height="20" viewBox="0 0 20 20">
+        <line x1="10" y1="14" x2="10" y2="6" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></line>
+        <polyline points="13 9 10 6 7 9" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></polyline>
+        <path d="m17,13v1c0,1.657-1.343,3-3,3h-1" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path>
+        <path d="m13,3h1c1.657,0,3,1.343,3,3v1" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path><path d="m3,7v-1c0-1.657,1.343-3,3-3h1" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path>
+        <path d="m7,17h-1c-1.657,0-3-1.343-3-3v-1" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path>
+      </svg>
+    `;
+    left.append(input, span);
+    el.append(left, right);
+    setTimeout(() => {
+      input.focus();
+    }, 0);
+    return el;
+  },
+});
 const virtualizer = new Virtualizer();
 const poolManager = createWorkerAPI({
   theme: DEFAULT_THEMES,
